0ce443bed5a7bdce32e69ee22bb14770d3f168ee
Gabriel <gabriel.rosser@gmail.com>
2014-08-21 17:31:53 +0100

    Switched pickle to dill to allow pickling of lambda functions when running validation.

--- setUp_for_validation.py
+++ setUp_for_validation.py
@@ -2,11 +2,11 @@ __author__ = 'gabriel'
 import numpy as np
 import datetime
 from analysis import plotting, chicago
-import pickle
+import dill
 
 
 with open('assess_by_one_month.pickle', 'w') as f:
-    callback = lambda data: pickle.dump(data, f)
+    callback = lambda data: dill.dump(data, f)
     res2 = chicago.validate_point_process_multi(
         start_date=datetime.datetime(2002, 1, 1),
         training_size=30,
@@ -18,27 +18,27 @@ with open('assess_by_one_month.pickle', 'w') as f:
     )
 
 
-with open('assess_by_three_months.pickle', 'w') as f:
-    callback = lambda data: pickle.dump(data, f)
-    res2 = chicago.validate_point_process_multi(
-        start_date=datetime.datetime(2002, 1, 1),
-        training_size=90,
-        num_validation=20,
-        num_pp_iter=20,
-        grid_size=500,
-        dt=90,
-        callback_func=callback
-    )
-
-
-with open('assess_by_year.pickle', 'w') as f:
-    callback = lambda data: pickle.dump(data, f)
-    res1 = chicago.validate_point_process_multi(
-        start_date=datetime.datetime(2002, 1, 1),
-        training_size=360,
-        num_validation=20,
-        num_pp_iter=20,
-        grid_size=500,
-        dt=180,
-        callback_func=callback
-    )
+# with open('assess_by_three_months.pickle', 'w') as f:
+#     callback = lambda data: pickle.dump(data, f)
+#     res2 = chicago.validate_point_process_multi(
+#         start_date=datetime.datetime(2002, 1, 1),
+#         training_size=90,
+#         num_validation=20,
+#         num_pp_iter=20,
+#         grid_size=500,
+#         dt=90,
+#         callback_func=callback
+#     )
+#
+#
+# with open('assess_by_year.pickle', 'w') as f:
+#     callback = lambda data: pickle.dump(data, f)
+#     res1 = chicago.validate_point_process_multi(
+#         start_date=datetime.datetime(2002, 1, 1),
+#         training_size=360,
+#         num_validation=20,
+#         num_pp_iter=20,
+#         grid_size=500,
+#         dt=180,
+#         callback_func=callback
+#     )
