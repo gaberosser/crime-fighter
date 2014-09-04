Made application of SEPP to chicago code more general Added methods to KDE for computing the marginal and partial variance in X and Y Fixed KDE tests and added some new ones for the new methods, though some still required (see TODO in comments) Added plotting routine to produce subplot of trigger marginals

analysis/chicago.py
@@ -98,12 +98,16 @@ def pairwise_time_lag_events(max_distance=200, num_bins=None, crime_type='burgla
     return dt
 
 
-def apply_point_process():
+def apply_point_process(start_date=datetime.datetime(2010, 3, 1, 0),
+                        end_date=datetime.datetime(2010, 6, 1, 0),
+                        domain=None,
+                        niter=15):
 
     res, t0 = get_crimes_by_type(
         crime_type='burglary',
-        start_date=datetime.datetime(2010, 3, 1, 0),
-        end_date=datetime.datetime(2010, 6, 1, 0)
+        start_date=start_date,
+        end_date=end_date,
+        domain=domain
     )
 
     max_trigger_t = 40 # units days
@@ -111,40 +115,19 @@ def apply_point_process():
     min_bandwidth = np.array([0.3, 5., 5.])
     # min_bandwidth = None
 
-    # manually estimate p initially
+    # define initial estimator
     est = lambda x, y: estimation.estimator_bowers(x, y, ct=1, cd=0.02)
+
+    # instantiate SEPP model
     r = pp_models.PointProcessStochasticNn(estimator=est, max_trigger_t=max_trigger_t, max_trigger_d=max_trigger_d,
                             min_bandwidth=min_bandwidth)
     # r = pp_models.PointProcessStochastic(estimator=est, max_trigger_t=max_trigger_t, max_trigger_d=max_trigger_d,
     #                         min_bandwidth=[1., 5., 5.])
-    ps = r.train(data=res, niter=12, tol_p=5e-5)
+
+    # train on ALL data
+    ps = r.train(data=res, niter=niter, tol_p=5e-5)
     return r, ps
 
-
-def apply_point_process_southwest():
-
-    southwest = models.ChicagoDivision.objects.get(name='Southwest').mpoly.buffer(1500)
-
-    res, t0 = get_crimes_by_type(
-        crime_type='burglary',
-        start_date=datetime.datetime(2009, 6, 1, 0),
-        end_date=datetime.datetime(2010, 6, 1, 0),
-        domain=southwest
-    )
-
-    max_trigger_t = 40 # units days
-    max_trigger_d = 300 # units metres
-    min_bandwidth = np.array([0.3, 5., 5.])
-    # min_bandwidth = None
-
-    # manually estimate p initially
-    est = lambda x, y: estimation.estimator_bowers(x, y, ct=1, cd=0.02)
-    r = pp_models.PointProcessStochasticNn(estimator=est, max_trigger_t=max_trigger_t, max_trigger_d=max_trigger_d,
-                            min_bandwidth=min_bandwidth)
-    # r = pp_models.PointProcessStochastic(estimator=est, max_trigger_t=max_trigger_t, max_trigger_d=max_trigger_d,
-    #                         min_bandwidth=[1., 5., 5.])
-    ps = r.train(data=res, niter=12, tol_p=5e-5)
-    return r, ps
 
 def validate_point_process(
         start_date=datetime.datetime(2001, 3, 1, 0),

kde/methods/pure_python.py
@@ -38,12 +38,31 @@ def marginal_icdf_optimise(k, y, dim=0, tol=1e-8):
 
 def weighted_stdev(data, weights):
     ndata = data.shape[0]
-    ndim = data.shape[1]
+    if weights.ndim != 1:
+        raise AttributeError("Weights must be a 1D array")
+    if weights.size != ndata:
+        raise AttributeError("Length of weights vector not equal to number of data")
+
+    if data.ndim == 1:
+        # 1D data
+        ndim = 1
+        _data = data.reshape(ndata, 1)
+    else:
+        ndim = data.shape[1]
+        _data = data
+
     tiled_weights = np.tile(weights.reshape(ndata, 1), (1, ndim))
     sum_weights = np.sum(weights)
-    M = weights.nonzero()[0].size
-    wm = np.sum(tiled_weights * data, axis=0) / sum_weights
-    return np.sqrt(np.sum((tiled_weights * (data - wm)) ** 2, axis=0) / sum_weights * M / (M - 1.))
+    M = float(sum(weights != 0.))  # number nonzero weights
+    wm = np.sum(tiled_weights * _data, axis=0) / sum_weights  # weighted arithmetic mean
+    a = np.sum(tiled_weights * ((_data - wm) ** 2), axis=0)  # numerator
+    b = sum_weights * (M - 1.) / M  # denominator
+
+    # check return type
+    if ndim == 1:
+        return np.sqrt(a / b)[0]
+    else:
+        return np.sqrt(a / b)
 
 
 class FixedBandwidthKde(object):
@@ -58,7 +77,6 @@ class FixedBandwidthKde(object):
         self.pool = None
 
     def set_bandwidths(self, *args, **kwargs):
-
         try:
             bandwidths = kwargs.pop('bandwidths')
         except KeyError:
@@ -96,7 +114,7 @@ class FixedBandwidthKde(object):
 
     @property
     def raw_std_devs(self):
-        return np.std(self.data, axis=0)
+        return np.std(self.data, axis=0, ddof=1)
 
     def _additive_operation(self, funcstr, *args, **kwargs):
         """ Generic interface to call function named in funcstr on the data, handling normalisation and reshaping """
@@ -156,6 +174,46 @@ class FixedBandwidthKde(object):
         for x in it:
             x[-1][...] = self.pdf(*x[:-1]) # NB must unpack, as x[:-1] is a tuple
         return it.operands[-1]
+
+    @property
+    def marginal_mean(self):
+        return np.mean(self.data, axis=0)
+
+    @property
+    def marginal_second_moment(self):
+        return np.sum(self.bandwidths ** 2 + self.data ** 2, axis=0) / float(self.ndata)
+
+    @property
+    def marginal_variance(self):
+        return self.marginal_second_moment - self.marginal_mean ** 2
+
+    def _t_dependent_variance(self, t):
+        ## TODO: test me!
+        # have already checked that ndim > 1 by this point
+        z0 = np.tile(
+            np.array([m.marginal_pdf(t, dim=0) for m in self.mvns]).reshape((self.ndata, 1)),
+            (1, self.ndim - 1)
+        )
+        tdm = np.mean(self.data[:, 1:] * z0, axis=0)
+        tdsm = np.mean((self.bandwidths[:, 1:] ** 2 + self.data[:, 1:] ** 2) * z0, axis=0)
+        tdv = tdsm - tdm ** 2
+
+        return tdm, tdsm, tdv
+
+    def t_dependent_mean(self, t):
+        if self.ndim == 1:
+            raise NotImplementedError("Unable to compute time-dependent mean with ndim=1")
+        return self._t_dependent_variance(t)[0]
+
+    def t_dependent_second_moment(self, t):
+        if self.ndim == 1:
+            raise NotImplementedError("Unable to compute time-dependent second moment with ndim=1")
+        return self._t_dependent_variance(t)[1]
+
+    def t_dependent_variance(self, t):
+        if self.ndim == 1:
+            raise NotImplementedError("Unable to compute time-dependent variance with ndim=1")
+        return self._t_dependent_variance(t)[2]
 
 
 class VariableBandwidthKde(FixedBandwidthKde):

kde/tests.py
@@ -1,7 +1,8 @@
 __author__ = 'gabriel'
 import unittest
 import kernels
-from methods.pure_python import VariableBandwidthKde, VariableBandwidthNnKde, FixedBandwidthKde, WeightedVariableBandwidthNnKde
+from methods.pure_python import VariableBandwidthKde, VariableBandwidthNnKde, FixedBandwidthKde, \
+    WeightedVariableBandwidthNnKde, weighted_stdev
 import numpy as np
 from scipy.stats import norm, multivariate_normal
 from scipy.integrate import quad, tplquad
@@ -20,6 +21,38 @@ class TestHelperFunctions(unittest.TestCase):
         self.assertAlmostEqual(kernels.normcdf(0., 0., 1.), 0.5)
         self.assertAlmostEqual(kernels.normcdf(10., 0., 1.), 1.0)
         self.assertAlmostEqual(kernels.normcdf(0., 0., 10.), 0.5)
+
+    def test_weighted_stdev(self):
+        # 1D data
+
+        # weights all unity
+        data = np.linspace(0, 1, 11)
+        weights = np.ones_like(data)
+        # test the weighted stdev is equal to the usual UNBIASED estimator
+        sw = weighted_stdev(data, weights)
+        self.assertIsInstance(sw, float)
+        self.assertEqual(sw, np.std(data, ddof=1))
+
+        # weights all equal
+        weights *= np.pi
+        # test the weighted stdev is equal to the usual UNBIASED estimator
+        self.assertEqual(weighted_stdev(data, weights), np.std(data, ddof=1))
+
+        # two source distributions (weights differ)
+        n = 1000000
+        weighted_data = np.hstack((np.random.RandomState(42).randn(n) - 1, np.random.RandomState(42).randn(n) + 1))
+        weights = np.hstack((np.ones(n) * 2 / 3., np.ones(n) / 3.))
+        sw = weighted_stdev(weighted_data, weights)
+
+        # analytic variance is 1 + 8/9
+        self.assertAlmostEqual(sw, np.sqrt(1 + 8/9.), places=2)  # 2DP
+
+        # 2D data
+        data = np.tile(np.linspace(0, 1, 11).reshape(11, 1), (1, 2))
+        weights = np.ones(11)
+        sw = weighted_stdev(data, weights)
+        self.assertEqual(sw.size, 2)
+        self.assertEqual(sw[0], sw[1])
 
 
 class TestMultivariateNormal(unittest.TestCase):
@@ -85,6 +118,12 @@ class TestFixedBandwidthKde(unittest.TestCase):
 
 class TestVariableBandwidthKde(unittest.TestCase):
 
+    def setUp(self):
+        d = np.meshgrid(*([[0, 3]]*3))
+        self.data_3d = np.vstack(tuple([x.flatten() for x in d])).transpose()
+        self.bd_3d = np.vstack(tuple([np.ones(3)+np.random.RandomState(42).rand(3) for i in range(1, 9)]))
+        self.kde = VariableBandwidthKde(self.data_3d, bandwidths=self.bd_3d)
+
     def test_kde_1d(self):
         data_1d = np.array([0, 3])
         bd_1d = [1., 2.]
@@ -103,63 +142,81 @@ class TestVariableBandwidthKde(unittest.TestCase):
         for y1, y2 in zip(y, y_expct):
             self.assertAlmostEqual(y1, y2)
 
-
     def test_kde_3d(self):
-        d = np.meshgrid(*([[0, 3]]*3))
-        data_3d = np.vstack(tuple([x.flatten() for x in d])).transpose()
-        bd_3d = np.vstack(tuple([np.ones(3)+np.random.random(3) for i in range(1,9)]))
-        kde = VariableBandwidthKde(data_3d, bandwidths=bd_3d)
-        self.assertIsInstance(kde.pdf([0., 1.], [0., 1.], [0., 1.]), np.ndarray)
+        self.assertEqual(self.kde.ndata, self.data_3d.shape[0])
+        self.assertIsInstance(self.kde.pdf([0., 1.], [0., 1.], [0., 1.]), np.ndarray)
         x = np.meshgrid(*([np.linspace(-1, 1, 10)]*3))
         xa = np.vstack((x[0].flatten(), x[1].flatten(), x[2].flatten())).transpose()
-        y = kde.pdf(*x)
+        y = self.kde.pdf(*x)
         y_expct = np.zeros((10, 10, 10))
-        for i in range(kde.ndata):
-            y_expct += multivariate_normal.pdf(xa, mean=data_3d[i, :], cov=np.eye(3) * bd_3d[i, :]**2).reshape((10, 10, 10))
-        y_expct /= kde.ndata
+        for i in range(self.kde.ndata):
+            y_expct += multivariate_normal.pdf(xa,
+                                               mean=self.data_3d[i, :],
+                                               cov=np.eye(3) * self.bd_3d[i, :]**2).reshape((10, 10, 10))
+        y_expct /= self.kde.ndata
         self.assertEqual(np.sum(np.abs((y - y_expct)).flatten()>1e-12), 0) # no single difference > 1e-12
 
         # UNNORMED
-        y = kde.pdf(*x, normed=False)
-        y_expct *= kde.ndata
+        y = self.kde.pdf(*x, normed=False)
+        y_expct *= self.kde.ndata
         self.assertEqual(np.sum(np.abs((y - y_expct)).flatten()>1e-12), 0) # no single difference > 1e-12
 
     def test_marginals(self):
-        d = np.meshgrid(*([[0, 3]]*3))
-        data_3d = np.vstack(tuple([x.flatten() for x in d])).transpose()
-        bd_3d = np.vstack(tuple([np.ones(3)+np.random.random(3) for i in range(1,9)]))
-        kde = VariableBandwidthKde(data_3d, bandwidths=bd_3d)
         x = np.linspace(-1, 1, 10)
 
         # check marginals in each dim
         for dim in range(3):
 
-            p = kde.marginal_pdf(x, dim=dim)
-            pu = kde.marginal_pdf(x, dim=dim, normed=False)
+            p = self.kde.marginal_pdf(x, dim=dim)
+            pu = self.kde.marginal_pdf(x, dim=dim, normed=False)
             p_expct = np.zeros(x.shape)
-            for i in range(kde.ndata):
-                p_expct += norm.pdf(x, loc=data_3d[i, dim], scale=bd_3d[i, dim])
-            p_expct /= kde.ndata
+            for i in range(self.kde.ndata):
+                p_expct += norm.pdf(x, loc=self.data_3d[i, dim], scale=self.bd_3d[i, dim])
+            p_expct /= self.kde.ndata
             self.assertEqual(np.sum(np.abs(p - p_expct) > 1e-12), 0) # no single difference > 1e-12
-            self.assertEqual(np.sum(np.abs(p * kde.ndata - pu) > 1e-12), 0)
+            self.assertEqual(np.sum(np.abs(p * self.kde.ndata - pu) > 1e-12), 0)
 
-            c = kde.marginal_cdf(x, dim=dim)
+            c = self.kde.marginal_cdf(x, dim=dim)
             c_expct = np.zeros(x.shape)
-            for i in range(kde.ndata):
-                c_expct += norm.cdf(x, loc=data_3d[i, dim], scale=bd_3d[i, dim])
-            c_expct /= kde.ndata
+            for i in range(self.kde.ndata):
+                c_expct += norm.cdf(x, loc=self.data_3d[i, dim], scale=self.bd_3d[i, dim])
+            c_expct /= self.kde.ndata
             self.assertEqual(np.sum(np.abs(c - c_expct) > 1e-12), 0) # no single difference > 1e-12
 
     def test_inverse_cdf(self):
-        d = np.meshgrid(*([[0, 3]]*3))
-        data_3d = np.vstack(tuple([x.flatten() for x in d])).transpose()
-        bd_3d = np.vstack(tuple([np.ones(3)+np.random.random(3) for i in range(1,9)]))
-        kde = VariableBandwidthKde(data_3d, bandwidths=bd_3d)
-
-        for y in [0.75, 0.9, 0.99, 0.9999]:
+        for y in [0.01, 0.1, 0.5, 0.9, 0.99, 0.9999]:
             for dim in range(2):
-                x = kde.marginal_icdf(y, dim=dim)
-                self.assertAlmostEqual(kde.marginal_cdf(x, dim=dim), y)
+                x = self.kde.marginal_icdf(y, dim=dim)
+                self.assertAlmostEqual(self.kde.marginal_cdf(x, dim=dim), y)
+
+    def test_moments(self):
+        eps = 1e-5  # results accurate to 3sf
+        lower = [0] * 3
+        upper = [0] * 3
+        lower[0] = self.kde.marginal_icdf(eps, dim=0)
+        upper[0] = self.kde.marginal_icdf(1 - eps, dim=0)
+        lower[1] = self.kde.marginal_icdf(eps, dim=1)
+        upper[1] = self.kde.marginal_icdf(1 - eps, dim=1)
+        lower[2] = self.kde.marginal_icdf(eps, dim=2)
+        upper[2] = self.kde.marginal_icdf(1 - eps, dim=2)
+
+        m1 = self.kde.marginal_mean
+        m2 = self.kde.marginal_second_moment
+        var = self.kde.marginal_variance
+
+        m1_computed = []
+        m2_computed = []
+        for i in range(3):
+
+            opt = lambda t: t * self.kde.marginal_pdf(t, dim=i)
+            m1_computed.append(quad(opt, lower[i], upper[i])[0])
+            self.assertAlmostEqual(m1_computed[-1], m1[i], places=2)  # NB places is DP
+
+            opt = lambda t: t ** 2 * self.kde.marginal_pdf(t, dim=i)
+            m2_computed.append(quad(opt, lower[i], upper[i])[0])
+            self.assertAlmostEqual(m2_computed[-1], m2[i], places=2)  # NB places is DP
+
+            self.assertAlmostEqual(m2_computed[-1] - m1_computed[-1] ** 2, var[i], places=2)
 
 
 class TestVariableBandwidthKdeNn(unittest.TestCase):
@@ -167,12 +224,14 @@ class TestVariableBandwidthKdeNn(unittest.TestCase):
     def test_kde_1d(self):
         data = np.linspace(0, 1, 11)
         kde = VariableBandwidthNnKde(data, nn=2)
-        nndiste = data[1] / np.std(data)
+        nndiste = data[1] / np.std(data, ddof=1)
         for n in kde.nn_distances:
             self.assertAlmostEqual(n, nndiste)
 
 
 class TestWeightedVariableBandwidthKdeNn(unittest.TestCase):
+
+    # NB weighted stdev tested in class TestHelperFunctions
 
     def test_kde_1d(self):
         data = np.linspace(0, 1, 11)

point_process/plotting.py
@@ -64,11 +64,13 @@ def plot_txy_kde(k, max_x, max_y, npt_1d=50, tpt=None, **kwargs):
     return fig
 
 
-def _plot_marginals(k, dim, norm=1.0, data_min=0., data_max=None, npt_1d=200, **kwargs):
+def _plot_marginals(k, dim, norm=1.0, data_min=None, data_max=None, npt_1d=200, **kwargs):
     style = kwargs.pop('style', 'k-')
-    # if data_max is missing, use the 95th percentile
+    # if data_max is missing, use the 99th percentile
     if data_max is None:
-        data_max = k.marginal_icdf(0.95, dim=dim)
+        data_max = k.marginal_icdf(0.99, dim=dim)
+    if data_min is None:
+        data_min = k.marginal_icdf(0.01, dim=dim)
     t = np.linspace(data_min, data_max, npt_1d)
     z = k.marginal_pdf(t, dim=dim, normed=False) / float(norm)
     if 'ax' in kwargs:
@@ -82,22 +84,24 @@ def _plot_marginals(k, dim, norm=1.0, data_min=0., data_max=None, npt_1d=200, **
     return fig, ax
 
 
-def plot_txy_t_marginals(k, norm=1.0, t_max=50, npt_1d=200, **kwargs):
-    fig, ax = _plot_marginals(k, 0, norm=norm, data_max=t_max, npt_1d=npt_1d, **kwargs)
+def plot_txy_t_marginals(k, norm=1.0, t_max=None, npt_1d=200, **kwargs):
+    fig, ax = _plot_marginals(k, 0, norm=norm, data_min=0., data_max=t_max, npt_1d=npt_1d, **kwargs)
     ax.set_xlabel('Time (days)')
     ax.set_ylabel('Density')
     return fig
 
 
-def plot_txy_x_marginals(k, norm=1.0, x_max=50, npt_1d=200, **kwargs):
-    fig, ax = _plot_marginals(k, 1, norm=norm, data_min=-x_max, data_max=x_max, npt_1d=npt_1d)
+def plot_txy_x_marginals(k, norm=1.0, x_max=None, npt_1d=200, **kwargs):
+    x_min = -x_max if x_max else None
+    fig, ax = _plot_marginals(k, 1, norm=norm, data_min=x_min, data_max=x_max, npt_1d=npt_1d, **kwargs)
     ax.set_xlabel('X (metres)')
     ax.set_ylabel('Density')
     return fig
 
 
-def plot_txy_y_marginals(k, norm=1.0, y_max=50, npt_1d=200, **kwargs):
-    fig, ax = _plot_marginals(k, 2, norm=norm, data_min=-y_max, data_max=y_max, npt_1d=npt_1d)
+def plot_txy_y_marginals(k, norm=1.0, y_max=None, npt_1d=200, **kwargs):
+    y_min = -y_max if y_max else None
+    fig, ax = _plot_marginals(k, 2, norm=norm, data_min=y_min, data_max=y_max, npt_1d=npt_1d, **kwargs)
     ax.set_xlabel('Y (metres)')
     ax.set_ylabel('Density')
     return fig
@@ -267,3 +271,18 @@ def multiplots(ppobj, simobj=None, maxes=None):
         ax.set_ylim([0, 1.05 * zmax_infer])
     ax.set_xlim([-y_max, y_max])
     ax.legend(ax.get_lines(), ('Inferred', 'True'), 'upper right')
+
+
+def plot_trigger_marginals(trigger_kde):
+    f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharex=False, sharey=False)
+    ax1.yaxis.set_ticklabels([])
+    ax1.yaxis.set_ticks([])
+    ax2.yaxis.set_ticklabels([])
+    ax2.yaxis.set_ticks([])
+    ax3.yaxis.set_ticklabels([])
+    ax3.yaxis.set_ticks([])
+
+    plot_txy_t_marginals(trigger_kde, ax=ax1)
+    plot_txy_x_marginals(trigger_kde, ax=ax2)
+    plot_txy_y_marginals(trigger_kde, ax=ax3)
+