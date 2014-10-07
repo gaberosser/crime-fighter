Fixed kde tests, validation tests. Added faster random sampling method based on a 2006 paper, seems to be around twice as fast with similar results. Finished faster NN distance computation too. TODO: left a few comments scattered around in SEPP validation method and chicago.validate_... because it fails, requiring vast amount of memory.

analysis/chicago.py
@@ -98,7 +98,10 @@ def pairwise_time_lag_events(max_distance=200, num_bins=None, crime_type='burgla
 def apply_point_process(start_date=datetime.datetime(2010, 3, 1, 0),
                         end_date=datetime.datetime(2010, 6, 1, 0),
                         domain=None,
-                        niter=15):
+                        niter=15,
+                        min_bandwidth=None,
+                        max_trigger_t=40,
+                        max_trigger_d=300):
 
     res, t0 = get_crimes_by_type(
         crime_type='burglary',
@@ -107,10 +110,8 @@ def apply_point_process(start_date=datetime.datetime(2010, 3, 1, 0),
         domain=domain
     )
 
-    max_trigger_t = 40 # units days
-    max_trigger_d = 300 # units metres
-    min_bandwidth = np.array([0.3, 5., 5.])
-    # min_bandwidth = None
+    # NB t is in units of days, space is in units of metres
+    # a starting point for min_bandwidth = np.array([0.3, 5., 5.])
 
     # define initial estimator
     est = lambda x, y: estimation.estimator_bowers(x, y, ct=1, cd=0.02)
@@ -122,12 +123,12 @@ def apply_point_process(start_date=datetime.datetime(2010, 3, 1, 0),
     #                         min_bandwidth=[1., 5., 5.])
 
     # train on ALL data
-    ps = r.train(data=res, niter=niter, tol_p=5e-5)
+    ps = r.train(data=res, niter=niter, tol_p=1e-5)
     return r, ps
 
 
 def validate_point_process(
-        start_date=datetime.datetime(2012, 3, 31, 0),
+        start_date=datetime.datetime(2012, 3, 1, 0),
         training_size=365,
         num_validation=20,
         num_pp_iter=20,
@@ -166,6 +167,7 @@ def validate_point_process(
     vb.set_grid(grid)
     vb.set_t_cutoff(training_size, b_train=False)
 
+    ## TODO: check the number of iterations reported is as expected here
     res = vb.run(time_step=prediction_dt, t_upper=training_size + ndays, true_dt_plus=true_dt_plus,
                  train_kwargs={'niter': num_pp_iter, 'tol_p': 1e-5},
                  verbose=True)

analysis/tests.py
@@ -279,11 +279,8 @@ class TestValidation(unittest.TestCase):
     def setUp(self):
         n = 100
         t = np.linspace(0, 1, n).reshape((n, 1))
-        np.random.shuffle(t)
         self.data = np.hstack((t, np.random.RandomState(42).rand(n, 2)))
         stk = hotspot.SKernelHistoric(1, bdwidth=0.3)
-        self.vb = validation.ValidationBase(self.data, hotspot.Hotspot, model_args=(stk,))
-        self.vb.train_model()
 
     def test_mcsampler(self):
         poly = geos.Polygon([
@@ -329,13 +326,17 @@ class TestValidation(unittest.TestCase):
 
     def test_instantiation(self):
 
+        # shuffle data to check that it is resorted
+        data = np.array(self.data)
+        np.random.shuffle(data)
+
         stk = hotspot.SKernelHistoric(1, bdwidth=0.3)
-        vb = validation.ValidationBase(self.data, hotspot.Hotspot, model_args=(stk,))
-        vb.train_model()
+        vb = validation.ValidationBase(data, hotspot.Hotspot, model_args=(stk,))
 
         # check data are present and sorted
         self.assertEqual(vb.ndata, self.data.shape[0])
         self.assertTrue(np.all(np.diff(vb.data[:, 0]) > 0))
+        self.assertTrue(np.all(vb.data == self.data))
 
         # no grid present yet as not supplied
         self.assertTrue(vb.roc.poly is None)
@@ -351,30 +352,33 @@ class TestValidation(unittest.TestCase):
             max(self.data[:, 2]),
         ])
 
+        # repeat but this time copy the grid
+        vb2 = validation.ValidationBase(self.data, hotspot.Hotspot, model_args=(stk,))
+        self.assertTrue(vb2.roc.poly is None)
+        vb2.set_grid(vb.roc)
+
+        self.assertListEqual(vb.roc.egrid, vb2.roc.egrid)
+
     def test_time_cutoff(self):
 
         stk = hotspot.SKernelHistoric(1, bdwidth=0.3)
         vb = validation.ValidationBase(self.data, hotspot.Hotspot, model_args=(stk,))
-        vb.train_model()
 
-        data = self.data[np.argsort(self.data[:, 0])]
         # expected cutoff automatically chosen
-        cutoff_te = data[int(self.data.shape[0] / 2), 0]
+        cutoff_te = vb.data[int(self.data.shape[0] / 2), 0]
 
         # check time cutoff and training/test split
         self.assertEqual(vb.cutoff_t, cutoff_te)
 
         # training set
-        training_expctd = data[data[:, 0] <= cutoff_te]
+        training_expctd = self.data[self.data[:, 0] <= cutoff_te]
         self.assertEqual(vb.training.shape[0], len(training_expctd))
-        for i in range(len(training_expctd)):
-            self.assertListEqual(list(vb.training[i]), list(data[i]))
+        self.assertTrue(np.all(vb.training == training_expctd))
 
         # testing set
-        testing_expctd = data[data[:, 0] > cutoff_te]
+        testing_expctd = self.data[self.data[:, 0] > cutoff_te]
         self.assertEqual(vb.testing().shape[0], len(testing_expctd))
-        for i in range(len(testing_expctd)):
-            self.assertListEqual(list(vb.testing()[i]), list(testing_expctd[i]))
+        self.assertTrue(np.all(vb.testing() == testing_expctd))
 
         # test when as_point=True
         tst = vb.testing(as_point=True)
@@ -387,26 +391,23 @@ class TestValidation(unittest.TestCase):
         # change cutoff_t
         cutoff_te = 0.3
         vb.set_t_cutoff(cutoff_te)
-        testing_expctd = data[data[:, 0] > cutoff_te]
+        testing_expctd = self.data[self.data[:, 0] > cutoff_te]
         self.assertEqual(vb.testing().shape[0], len(testing_expctd))
-        for i in range(len(testing_expctd)):
-            self.assertListEqual(list(vb.testing()[i]), list(testing_expctd[i]))
+        self.assertTrue(np.all(vb.testing() == testing_expctd))
 
         # testing dataset with dt_plus specified
         dt_plus = testing_expctd[2, 0] - cutoff_te
         testing_expctd = testing_expctd[:3]  # three results
         self.assertEqual(vb.testing(dt_plus=dt_plus).shape[0], len(testing_expctd))
-        for i in range(len(testing_expctd)):
-            self.assertListEqual(list(vb.testing(dt_plus=dt_plus)[i]), list(testing_expctd[i]))
+        self.assertTrue(np.all(vb.testing(dt_plus=dt_plus) == testing_expctd))
 
         # testing dataset with dt_plus and dt_minus
-        dt_plus = data[data[:, 0] > cutoff_te][17, 0] - cutoff_te
-        dt_minus = data[data[:, 0] > cutoff_te][10, 0] - cutoff_te
-        testing_expctd = data[(data[:, 0] > (cutoff_te + dt_minus)) & (data[:, 0] <= (cutoff_te + dt_plus))]
+        dt_plus = self.data[self.data[:, 0] > cutoff_te][17, 0] - cutoff_te
+        dt_minus = self.data[self.data[:, 0] > cutoff_te][10, 0] - cutoff_te
+        testing_expctd = self.data[(self.data[:, 0] > (cutoff_te + dt_minus)) & (self.data[:, 0] <= (cutoff_te + dt_plus))]
         self.assertEqual(vb.testing(dt_plus=dt_plus, dt_minus=dt_minus).shape[0], 7)
         self.assertEqual(vb.testing(dt_plus=dt_plus, dt_minus=dt_minus).shape[0], len(testing_expctd))
-        for i in range(len(testing_expctd)):
-            self.assertListEqual(list(vb.testing(dt_plus=dt_plus, dt_minus=dt_minus)[i]), list(testing_expctd[i]))
+        self.assertTrue(np.all(vb.testing(dt_plus=dt_plus, dt_minus=dt_minus) == testing_expctd))
 
     def test_training(self):
 
@@ -441,16 +442,10 @@ class TestValidation(unittest.TestCase):
         vb.train_model()
         vb.set_grid(0.1)
 
-        # predict at centroid of domain
-        pop = vb.predict_on_poly(vb.cutoff_t, vb.spatial_domain)
-        centroid = vb.spatial_domain.centroid.coords
-        pope = vb.model.predict(vb.cutoff_t, *centroid)
-        self.assertEqual(pop, pope)
-
         # spoof predict at all grid centroids
         stk = mock.create_autospec(hotspot.STKernelBowers)
         vb.model = stk
-        pop = vb.predict(self.vb.cutoff_t)
+        pop = vb.predict(vb.cutoff_t)
 
         self.assertEqual(stk.predict.call_count, 1)
         self.assertEqual(len(stk.predict.call_args[0]), 3)
@@ -471,7 +466,7 @@ class TestValidation(unittest.TestCase):
         self.assertEqual(stk.ndata, 0)
         vb.train_model()
         vb.set_grid(0.1)
-        pop = vb.predict(self.vb.cutoff_t)
+        pop = vb.predict(vb.cutoff_t)
         self.assertEqual(len(pop), len(vb.centroids))
         pope = np.array([np.sum(self.data[self.data[:, 0] <= vb.cutoff_t, 1] - x.centroid.coords[0]) for x in vb.roc.igrid])
         for (p, pe) in zip(pop, pope):
@@ -516,38 +511,59 @@ class TestValidation(unittest.TestCase):
         self.assertTrue(vb.roc.evaluate.called)
         self.assertEqual(vb.roc.evaluate.call_count, 1)
 
-    def test_run(self):
-
+    def test_run_calls_iterate_run(self):
         stk = hotspot.SKernelHistoric(1, bdwidth=0.3)
+
+        # check correct calls are made to _iterate_run with no t_upper
         with mock.patch.object(validation.ValidationBase, '_iterate_run',
                                return_value=collections.defaultdict(list)) as m:
             vb = validation.ValidationBase(self.data, hotspot.Hotspot, model_args=(stk,))
+            vb.set_grid(0.1)
             t0 = vb.cutoff_t
-            res = vb.run(time_step=0.1)
-            expct_call_count = math.ceil((1 - vb.cutoff_t) / 0.1)
+            vb.run(time_step=0.1)
+            expct_call_count = math.ceil((1 - t0) / 0.1)
             self.assertEqual(m.call_count, expct_call_count)
             for c in m.call_args_list:
                 self.assertEqual(c, mock.call(0.1, 0.1, 0.))  # signature: pred_dt_plus, true_dt_plus, true_dt_minus
-            # check that cutoff time is reset correctly
-            self.assertEqual(vb.cutoff_t, t0)
 
+    def test_run_calls_initial_setup(self):
+        stk = hotspot.SKernelHistoric(1, bdwidth=0.3)
+
+        # need to train model before running, otherwise it won't get past the call to the mocked function
+        vb = validation.ValidationBase(self.data, hotspot.Hotspot, model_args=(stk,))
+        vb.set_grid(0.1)
+        vb.train_model()
+
+        # check correct calls being made to _initial_setup
         with mock.patch.object(validation.ValidationBase, '_initial_setup') as m:
-            vb = validation.ValidationBase(self.data, hotspot.Hotspot, model_args=(stk,))
-            vb.set_grid(0.1)
+
             t0 = vb.cutoff_t
             res = vb.run(time_step=0.1)
-            self.assertEqual(m.call_count, 1)
-            self.assertEqual(m.call_args, mock.call())
+            self.assertEqual(m.call_count, 1)  # initial setup only called once
+            self.assertEqual(m.call_args, mock.call())  # no arguments passed
+
+            # test passing training keywords
             train_kwargs = {'foo': 'bar'}
             res = vb.run(time_step=0.1, train_kwargs=train_kwargs)
             self.assertEqual(m.call_args, mock.call(**train_kwargs))
 
+    def test_run_calls_update(self):
+        stk = hotspot.SKernelHistoric(1, bdwidth=0.3)
+
+        # need to train model before running, otherwise it won't get past the call to the mocked function
+        vb = validation.ValidationBase(self.data, hotspot.Hotspot, model_args=(stk,))
+        vb.set_grid(0.1)
+        vb.train_model()
+
+        # check correct calls being made to _update
         with mock.patch.object(validation.ValidationBase, '_update') as m:
-            vb = validation.ValidationBase(self.data, hotspot.Hotspot, model_args=(stk,))
-            vb.set_grid(0.1)
             t0 = vb.cutoff_t
             res = vb.run(time_step=0.1)
-            expct_call_count = math.ceil((1 - vb.cutoff_t) / 0.1)
+
+            # _update is the time updater, so cutoff time should not have changed
+            self.assertEqual(vb.cutoff_t, t0)
+
+            expct_call_count = math.ceil((1 - t0) / 0.1)
             self.assertEqual(m.call_count, expct_call_count)
             self.assertEqual(m.call_args_list[0], mock.call(time_step=0.0))  # _initial_setup routes to here
             self.assertEqual(m.call_args, mock.call(0.1))  # normal operation
@@ -556,12 +572,17 @@ class TestValidation(unittest.TestCase):
         vb = validation.ValidationBase(self.data, hotspot.Hotspot, model_args=(stk,))
         expct_call_count = math.ceil((1 - vb.cutoff_t) / 0.1)
 
+    def test_run_no_grid(self):
+        stk = hotspot.SKernelHistoric(1, bdwidth=0.3)
+        vb = validation.ValidationBase(self.data, hotspot.Hotspot, model_args=(stk,))
+        t0 = vb.cutoff_t
         # no grid specified raises error
         with self.assertRaises(AttributeError):
             res = vb.run(time_step=0.1)
         vb.set_grid(0.1)
         res = vb.run(time_step=0.1)
 
+        expct_call_count = math.ceil((1 - t0) / 0.1)
         for v in res.values():
             self.assertEqual(len(v), expct_call_count)
 

analysis/validation.py
@@ -18,7 +18,7 @@ def mc_sampler(poly):
 
 class ValidationBase(object):
 
-    def __init__(self, data, model_class, spatial_domain=None, grid_length=None, tmax_initial=None, model_args=None, model_kwargs=None):
+    def __init__(self, data, model_class, spatial_domain=None, grid_length=None, cutoff_t=None, model_args=None, model_kwargs=None):
         # sort data in increasing time
         self.data = data
         self.data = np.array(data)[np.argsort(self.t)]
@@ -28,7 +28,7 @@ class ValidationBase(object):
         self.model = model_class(*self.model_args, **self.model_kwargs)
 
         # set initial time cut point
-        self.cutoff_t = tmax_initial or self.t[int(self.ndata / 2)]
+        self.cutoff_t = cutoff_t or self.t[int(self.ndata / 2)]
 
         # set roc
         self.roc = None
@@ -109,27 +109,13 @@ class ValidationBase(object):
         """ Train the predictor on training data """
         self.model.train(self.training, *args, **kwargs)
 
-    # def predict_on_poly(self, t, poly, *args, **kwargs):
-    #     # FIXME: disabled as too slow
-    #     # idea here is to allow more accurate assessment of prediction on a region, rather than just using the centroid
-    #     method = kwargs.pop('method', 'centroid')
-    #     if method == 'int':
-    #         res, err = mcint.integrate(lambda x: self.model.predict(t, x[0], x[1]), mc_sampler(poly), n=100)
-    #     elif method == 'centroid':
-    #         res = self.model.predict(t, *poly.centroid.coords)
-    #     else:
-    #         raise NotImplementedError("Unsupported method %s", method)
-    #     return res
-
     def predict(self, t, **kwargs):
-        print "predict"
         # estimate total propensity in each grid poly
         # use centroid method for speed
         ts = np.ones(len(self.roc.egrid)) * t
         return self.model.predict(ts, self.centroids[:, 0], self.centroids[:, 1])
 
     def _iterate_run(self, pred_dt_plus, true_dt_plus, true_dt_minus, **kwargs):
-        print "_iterate_run"
         true_dt_plus = true_dt_plus or pred_dt_plus
         prediction = self.predict(self.cutoff_t + pred_dt_plus, **kwargs)
         testing_data = self.testing(dt_plus=true_dt_plus, dt_minus=true_dt_minus)
@@ -138,7 +124,13 @@ class ValidationBase(object):
 
         return self.roc.evaluate()
 
-    def run(self, time_step, pred_dt_plus=None, true_dt_plus=None, true_dt_minus=0, t_upper=None, pred_kwargs=None,
+    def run(self, time_step,
+            pred_dt_plus=None,
+            true_dt_plus=None,
+            true_dt_minus=0,
+            t_upper=None,
+            n_iter=None,
+            pred_kwargs=None,
             train_kwargs=None, **kwargs):
         """
         Run the required train / predict / assess sequence
@@ -154,16 +146,23 @@ class ValidationBase(object):
         :param kwargs: kwargs for the run function itself
         :return: results dictionary.
         """
-        print "run"
+        if bool(t_upper) and bool(n_iter):
+            raise AttributeError("Both t_upper AND n_iter were supplied, but only one is supported")
+
         verbose = kwargs.pop('verbose', True)
         pred_kwargs = pred_kwargs or {}
         train_kwargs = train_kwargs or {}
         pred_dt_plus = pred_dt_plus or time_step
         true_dt_plus = true_dt_plus or pred_dt_plus
-        t_upper = min(t_upper or np.inf, self.testing()[-1, 0])
 
-        # precompute number of iterations
-        n_iter = math.ceil((t_upper - self.cutoff_t) / time_step)
+        if t_upper:
+            n_iter = math.ceil((t_upper - self.cutoff_t) / time_step)
+        elif n_iter:
+            # check that this number of iterations is possible
+            if (self.cutoff_t + (n_iter - 1) * time_step + true_dt_minus) > self.data[-1, 0]:
+                raise AttributeError("The requested number of iterations is too great for the supplied data.")
+        else:
+            n_iter = math.ceil((self.data[-1, 0] - self.cutoff_t) / time_step)
 
         res = collections.defaultdict(list)
 
@@ -311,11 +310,11 @@ if __name__ == "__main__":
     data = data[data[:, 0].argsort()]
     vb = ValidationBase(data, pp_models.PointProcess,
                         model_kwargs={'max_trigger_t': 80, 'max_trigger_d': 0.75})
-    vb.set_grid(grid_length=5)
+    vb.set_grid(5)
     vb.train_model(niter=20)
 
 
-    rank, res, carea, cfrac, pai = vb.predict_assess(pred_dt_plus=1) # predict one day ahead
+    rank, res, carea, cfrac, pai = vb.run(time_step=1) # predict one day ahead
     polys = [vb.grid[i] for i in rank]
     n = vb.true_values(dt_plus=1, dt_minus=0)
 

kde/models.py
@@ -8,7 +8,7 @@ import multiprocessing as mp
 from kde import kernels
 from stats.logic import weighted_stdev
 from sklearn.neighbors import NearestNeighbors
-
+import ipdb  # just in case
 
 def marginal_icdf_optimise(k, y, dim=0, tol=1e-8):
     n = 100
@@ -393,8 +393,14 @@ class VariableBandwidthNnKde(VariableBandwidthKde):
         std = self.raw_std_devs
 
         # increment NN by one since first result is always self-match
-        nn_obj = NearestNeighbors(self.nn + 1).fit(nd)
-        dist, _ = nn_obj.kneighbors(nd)
+        from time import time
+        tic = time()
+        try:
+            nn_obj = NearestNeighbors(self.nn + 1).fit(nd)
+            dist, _ = nn_obj.kneighbors(nd)
+        except Exception as exc:
+            ipdb.set_trace()
+        print "NN computation complete in %f s" % (time() - tic)
 
         self.nn_distances = dist[:, -1]
 
@@ -402,52 +408,23 @@ class VariableBandwidthNnKde(VariableBandwidthKde):
             raise AttributeError("Encountered np.inf values in NN distances")
 
         # check for NN distances below tolerance
-        for i in np.where(self.nn_distances < tol)[0]:
-            import ipdb; ipdb.set_trace()
-            # for each point, perform a NN distance lookup on ALL valid NNs and use the first one above tol
-            datum = nd[i]
+        intol_idx = np.where(self.nn_distances < tol)[0]
+        if len(intol_idx):
+            intol_data = nd[intol_idx]
             nn_obj.n_neighbors = self.ndata
-            dist, _ = nn_obj.kneighbors(datum)
-            dist = dist[dist[self.nn + 1:] > tol]
-            if not len(dist):
-                raise AttributeError("Could not find a NN distance for datum %d that is above the min tolerance" % i)
-            self.nn_distances[i] = min(dist)
+            dist, _ = nn_obj.kneighbors(intol_data)
+            # for each point, perform a NN distance lookup on ALL valid NNs and use the first one above tol
+            for i, j in enumerate(intol_idx):
+                d = dist[i][self.nn + 1:]
+                self.nn_distances[j] = d[d > tol][0]
 
         self.nn_distances = self.nn_distances.reshape((self.ndata, 1))
         self.bandwidths = std * self.nn_distances
 
-        ## OLD METHOD
-
-        # from scipy.spatial import KDTree
-        # kd = KDTree(nd)
-        #
-        # self.nn_distances = np.zeros(self.ndata)
-        # self.bandwidths = np.zeros((self.ndata, self.ndim))
-        #
-        # for i in range(self.ndata):
-        #     d, _ = kd.query(nd[i, :], k=self.nn)
-        #     self.nn_distances[i] = max(d[~np.isinf(d)]) if np.isinf(d[-1]) else d[-1]
-        #
-        #     # check for zero distances
-        #     if self.nn_distances[i] < tol:
-        #         if default_distance:
-        #             self.nn_distances[i] = default_distance
-        #         else:
-        #             d, _ = kd.query(nd[i, :], k=kd.n) # all NN distance values
-        #             idx = (~np.isinf(d)) & (d > tol)
-        #             if np.any(idx):
-        #                 self.nn_distances[i] = d[np.where(idx)[0][0]]
-        #             else:
-        #                 raise AttributeError("No non-zero and finite NN distances available, and no default specified")
-        #
-        #     self.bandwidths[i] = std * self.nn_distances[i]
-
         # apply minimum bandwidth constraint if required
         if min_bandwidth is not None and np.any(self.bandwidths < min_bandwidth):
-            import ipdb; ipdb.set_trace()
             fix_idx = np.where(self.bandwidths < min_bandwidth)
-            rep_min = np.tile(min_bandwidth, (self.ndata, 1))
-            self.bandwidths[fix_idx] = rep_min[fix_idx]
+            self.bandwidths[fix_idx] = np.array(min_bandwidth)[fix_idx[1]]
 
 
 class WeightedFixedBandwidthKde(FixedBandwidthKde):

point_process/models.py
@@ -101,10 +101,13 @@ class PointProcess(object):
         ndata = data.shape[0]
 
         chunksize = min(chunksize, ndata * (ndata - 1) / 2)
+        ## FIXME: this next line will consume vast amount of memory in large datasets.
+        ## switch to a GENERATOR
         idx_i, idx_j = estimation.pairwise_differences_indices(ndata)
         link_i = []
         link_j = []
 
+        ## similarly, generator needed here (xrange?)
         for k in range(0, len(idx_i), chunksize):
             i = idx_i[k:(k + chunksize)]
             j = idx_j[k:(k + chunksize)]
@@ -138,6 +141,9 @@ class PointProcess(object):
         )
 
     def sample_data(self):
+        """
+        Naive weighted sampling algorithm
+        """
         bg_idx = []
         cause_idx = []
         effect_idx = []
@@ -161,6 +167,23 @@ class PointProcess(object):
                 effect_idx.append(effect)
 
         return bg_idx, cause_idx, effect_idx
+
+    def sample_data_efraimidis(self):
+        """
+        More optimised weighted sampling algorithm
+        """
+        urvs = np.random.random(self.p.nnz)
+        ks_matrix = self.p.copy()
+        ks_matrix.data = np.power(urvs, 1. / self.p.data)
+
+        # find the largest value in each column
+        causes = [self.linkage_cols[n][np.argmax(ks_matrix[:, n].data)] for n in range(self.ndata)]
+        effects = range(self.ndata)
+
+        bg_idx = [x for x, y in zip(causes, effects) if x == y]
+        cause_idx, effect_idx = zip(*[(x, y) for x, y in zip(causes, effects) if x != y])
+
+        return bg_idx, list(cause_idx), list(effect_idx)
 
     # def delete_overlaps(self):
     #     """ Prevent lineage links with exactly overlapping entries """
@@ -291,20 +314,26 @@ class PointProcess(object):
         if sparse.tril(self.p, k=-1).nnz != 0:
             raise AttributeError("Matrix P failed requirement that lower diagonal is zero.")
 
+        tic = time()
         self._set_kdes()
+        print "self._set_kdes() in %f s" % (time() - tic)
 
         # strip spatially overlapping points from p
         # self.delete_overlaps()
 
         # evaluate BG at data points
+        tic = time()
         m = self.background_density(self.data[:, 0], self.data[:, 1], self.data[:, 2])
+        print "self.background_density() in %f s" % (time() - tic)
 
         # evaluate trigger KDE at all interpoint distances
+        tic = time()
         trigger = self.trigger_density(
             self.interpoint_distance_data[:, 0],
             self.interpoint_distance_data[:, 1],
             self.interpoint_distance_data[:, 2],
             )
+        print "self.trigger_density() in %f s" % (time() - tic)
         g = sparse.csr_matrix((trigger, self.linkage), shape=(self.ndata, self.ndata))
 
         # recompute P
@@ -391,8 +420,11 @@ class PointProcessStochastic(PointProcess):
 class PointProcessStochasticNn(PointProcess):
 
     def _set_kdes(self):
-        bg_idx, cause_idx, effect_idx = self.sample_data()
+        tic = time()
+        bg_idx, cause_idx, effect_idx = self.sample_data_efraimidis()
+        # bg_idx, cause_idx, effect_idx = self.sample_data()
         interpoint = self.data[effect_idx] - self.data[cause_idx]
+        print "self.sample_data() in %f s" % (time() - tic)
 
         # compute KDEs
         try: