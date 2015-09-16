32ffe1343a8137a889a3bbce36fc0bba26f9a301
Gabriel <gabriel.rosser@gmail.com>
2015-09-16 17:44:54 +0100

    Started refactoring the spatial aggregation work (i.e. the time trend plots) for last revision of MA paper. Looks like a lot of the old analysis code like CadByGrid is broken. This should replace it, though it needs a time aggregation (counts by grid) class too. TODO: what did the xaxis major tick setting do?

--- analysis/ma_paper_figures.py
+++ analysis/ma_paper_figures.py
@@ -1,6 +1,7 @@
 __author__ = 'gabriel'
 
 import cad
+import chicago
 from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.transforms as mtransforms
@@ -8,74 +9,122 @@ from matplotlib import ticker
 import datetime
 import numpy as np
 from database import models
+from spacetime import DailyAggregate
+
+
+def moving_average(a, n=3):
+    assert n % 2 == 1, "Moving window size must be odd"
+    w = (n - 1) / 2
+    c = np.cumsum(a)
+    f = (c[w:] - c[:-w])[:-w] / float(n)
+    f = np.concatenate((np.nan * np.zeros(w), f, np.nan * np.zeros(w)))
+    return f
+
+
+def temporal_trend_plot(aggregate_obj, ax=None, shaded_region=None, smoothing_window=7):
+    """ aggregate_obj is an instance of TemporalAggregate """
+    ax = ax or plt.gca()
+
+    # setup x axis: assume
+    # x = [t.date() for t in aggregate_obj.keys()]
+    x = aggregate_obj.keys()
+    y = [len(t) for t in aggregate_obj.values()]
+    y_smooth = moving_average(y, n=smoothing_window)
+
+    # manual tick formatting
+    # tick_int = (5, 4, 3)
+
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_minor_locator(mdates.WeekdayLocator(byweekday=mdates.MO))
+    # ax.yaxis.set_major_locator(ticker.MultipleLocator(tick_int[i]))
+    ax.plot(x, y, 'k-', lw=1.5)
+    ax.plot(x, y_smooth, 'r-', lw=1.5, alpha=0.4)
+    fig.autofmt_xdate()
+    ax.grid(True, which='minor', axis='x', lw=1.5)
+    ax.tick_params(which='major', axis='x', length=7, width=2, labelsize=12)
+    ax.tick_params(which='major', axis='y', labelsize=12)
+
+    if shaded_region is not None:
+        fill_region = [True if shaded_region[0] <= d <= shaded_region[1] else False for d in x]
+        trans = mtransforms.blended_transform_factory(ax.transData, ax.transAxes)
+        ax.fill_between(x, 0, 1, where=fill_region, facecolor='k', alpha=0.3, transform=trans)
 
 
 """
 Trend line (temporal)
 """
 
+## Camden
 
-def moving_average(a, n=3) :
-    ret = np.cumsum(a, dtype=float)
-    ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1:] / n
+start_date = datetime.datetime(2011, 3, 1)
+predict_start_date = datetime.datetime(2011, 9, 28)
+end_date = predict_start_date + datetime.timedelta(days=99)
 
-
-cd = {}
-cd[1] = cad.CadDaily(nicl_number=1)  # violence
-cd[3] = cad.CadDaily(nicl_number=3)  # burglary
-cd[13] = cad.CadDaily(nicl_number=13)  # shoplifting
-
-# setup x axis
-x = [t.date() for t in cd[1].data.keys()]
-y = dict(
-    [(k, [len(t) for t in cd[k].data.values()]) for k in [1, 3, 13]]
+nicl_types = (
+    (1, 'Violence'),
+    (3, 'Burglary'),
+    (13, 'Shoplifting')
 )
 
-smoothed = []
+cad_data = {}
 
-for r in y.values():
-    res = moving_average(r, n=31)
-    # append buffers
-    smoothed.append(np.concatenate((np.nan * np.zeros(15), res, np.nan * np.zeros(15))))
+for k, v in nicl_types:
+    a, b, c = cad.get_crimes_by_type(k, convert_dates=False)
+    cad_data[v] = DailyAggregate(a, end_date=end_date)
 
-start_date = datetime.date(2011, 9, 28)
-end_date = start_date + datetime.timedelta(days=99)
-fill_region = [True if start_date <= d <= end_date else False for d in x]
 
-# manual tick formatting
-tick_int = (5, 4, 3)
-
-# fig = plt.figure()
 fig, axs = plt.subplots(3, sharex=True, sharey=False, figsize=(7.0, 4.5))
 big_ax = fig.add_subplot(111)
 big_ax.set_axis_bgcolor('none')
 big_ax.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
 big_ax.set_frame_on(False)
 
-for i, k in enumerate([1, 3, 13]):
-    # ax = fig.add_subplot(3, 1, i+1)
+for i, (k, da) in enumerate(cad_data.iteritems()):
     ax = axs[i]
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
-    ax.xaxis.set_major_locator(mdates.MonthLocator())
-    ax.xaxis.set_minor_locator(mdates.WeekdayLocator(byweekday=mdates.MO))
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(tick_int[i]))
-    ax.plot(x, y[k], 'k-', lw=1)
-    ax.plot(x, smoothed[i], 'r-', lw=1.5, alpha=0.4)
-    fig.autofmt_xdate()
-    ax.grid(True, which='minor', axis='x', lw=1.5)
-    ax.tick_params(which='major', axis='x', length=7, width=2, labelsize=12)
-    ax.tick_params(which='major', axis='y', labelsize=12)
-
-
-    trans = mtransforms.blended_transform_factory(ax.transData, ax.transAxes)
-    ax.fill_between(x, 0, 1, where=fill_region, facecolor='k', alpha=0.3, transform=trans)
+    temporal_trend_plot(da.data, ax=ax, shaded_region=[predict_start_date, end_date])
 
 big_ax.set_ylabel('Daily crime count', fontsize=12)
 
 for ax in axs:
     bbox = ax.get_position()
     ax.set_position([bbox.x0, bbox.y0, 1 - bbox.x0 - 0.02, bbox.height])
+
+
+## Chicago South
+
+chic_crime_types = (
+    ('burglary', 'Burglary'),
+    ('assault', 'Assault'),
+    ('motor vehicle theft', 'Motor vehicle theft')
+)
+chic_s = chicago.get_chicago_side_polys()['South']
+
+chic_data = {}
+for k, v in chic_crime_types:
+    a, b, c = chicago.get_crimes_by_type(crime_type=k,
+                                         convert_dates=False,
+                                         domain=chic_s,
+                                         start_date=start_date,
+                                         end_date=end_date)
+    chic_data[v] = DailyAggregate(a, end_date=end_date)
+
+fig, axs = plt.subplots(3, sharex=True, sharey=False, figsize=(7.0, 4.5))
+big_ax = fig.add_subplot(111)
+big_ax.set_axis_bgcolor('none')
+big_ax.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
+big_ax.set_frame_on(False)
+
+for i, (k, da) in enumerate(chic_data.iteritems()):
+    ax = axs[i]
+    temporal_trend_plot(da.data, ax=ax, shaded_region=[predict_start_date, end_date])
+
+big_ax.set_ylabel('Daily crime count', fontsize=12)
+
+for ax in axs:
+    bbox = ax.get_position()
+    ax.set_position([bbox.x0, bbox.y0, 1 - bbox.x0 - 0.02, bbox.height])
+
 
 """
 Spatial distribution
