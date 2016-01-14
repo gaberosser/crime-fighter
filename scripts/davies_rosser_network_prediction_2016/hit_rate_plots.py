76121895597fc97ee56cc15f4fdc5166b9561857
Gabriel Rosser <gabriel.rosser@gmail.com>
2016-01-14 11:13:02 +0000

    New hit rate plots for JQC paper

---
+++ scripts/davies_rosser_network_prediction_2016/hit_rate_plots.py
@@ -0,0 +1,71 @@
+__author__ = 'gabriel'
+from matplotlib import pyplot as plt
+from scripts import OUT_DIR
+import os
+from bisect import bisect_left
+import dill
+import numpy as np
+
+
+files = (
+    'network_hit_rates.dill',
+    'planar_grid_hit_rates.dill',
+    'planar_net_hit_rates.dill',
+)
+subdir = os.path.join(OUT_DIR, 'birmingham')
+
+
+if __name__ == '__main__':
+
+    coverage_levels = (0.01, 0.05, 0.1)
+
+    x = []
+    y = []
+    xm = []
+    ym = []
+    for fn in files:
+        with open(os.path.join(subdir, fn), 'r') as f:
+            tmp = dill.load(f)
+            x.append(tmp['x'])
+            y.append(tmp['y'])
+            xm.append(tmp['x'].mean(axis=0))
+            ym.append(tmp['y'].mean(axis=0))
+
+    # fig: mean hit rates
+    styles = ('k-', 'b-', 'r-')
+    leg = ('Network', 'Planar-grid', 'Planar-network')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for i in range(3):
+        ax.plot(xm[i], ym[i], styles[i])
+    ax.legend(leg, loc='upper left', frameon=False)
+    ax.set_xlim([0., 0.2])
+    ax.set_ylim([0., 0.6])
+    ax.set_xlabel('Coverage fraction')
+    ax.set_ylabel('Fraction crime captured')
+
+    # fig: improvement factor
+    titles = ('1\% coverage',
+              '5\% coverage',
+              '10\% coverage')
+    bins = np.linspace(-6, 6, 30)
+    fig, axs = plt.subplots(3, 1, sharex=True, figsize=(5, 6))
+    for i in range(3):
+        idx_net = bisect_left(xm[0], coverage_levels[i])
+        idx_grid = bisect_left(xm[2], coverage_levels[i])
+        ny = y[0][:, idx_net]
+        gy = y[2][:, idx_grid]
+        b = ym[2][idx_grid]
+        ax = axs[i]
+        rp = (ny - gy) / b
+        h = ax.hist(rp, bins)
+        # add dashed line for mean
+        c = ax.get_ylim()[1]
+        d = rp.mean()
+        ax.plot([d, d], [0, c], 'r--')
+
+    axs[2].set_xlabel('Network:planar relative daily hit rate')
+    axs[1].set_ylabel('Count')
+    plt.tight_layout()
+
+
