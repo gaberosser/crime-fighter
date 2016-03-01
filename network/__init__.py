02de058c0bfe9b1d5d9f7179465cf76b00c84221
Gabriel <gabriel.rosser@gmail.com>
2016-03-01 13:31:58 +0000

    Remove existing network files to be replaced with separate repo

--- network/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'gabriel'
-import os
-
-this_dir = os.path.dirname(os.path.realpath(__file__))
-TEST_DATA_FILE = os.path.join(this_dir, 'test_data', 'mastermap-itn_417209_0_brixton_sample.gml')
